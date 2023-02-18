5084a3570aa2ea634b03ceae07c6dad0c4ff0762
Micaella <micaella.comercial@gmail.com>
2023-02-18 13:51:34 -0300

    Subindo primeira versao do projeto petshop control

---
+++ petshop-control/petshop-control/src/main/java/com/api/petshopcontrol/PetshopControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.petshopcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopControlApplication.class, args);
+	}
+
+}
